20a50b0abbe9129f365978a42034475651ba73cd
Sergey Pistsov <pistsovsergey@outlook.com>
2021-09-28 22:01:41 +0500

    Добавьте файлы проекта.

---
+++ pascal_compiler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pascal_compiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
